b0d8848c5f1a320999255be6ccbf253baa365bcc
Davis Allie <davis525@live.com.au>
2016-01-16 14:46:37 +1100

    Starter Project Complete

---
+++ Custom Transitions/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  Custom Transitions
+//
+//  Created by Davis Allie on 16/01/2016.
+//  Copyright © 2016 tutsplus. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPanDown(sender: UIPanGestureRecognizer) {
+        
+    }
+
+    @IBAction func didPressToClose(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+}
